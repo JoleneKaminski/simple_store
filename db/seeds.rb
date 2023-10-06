4bb1cc82fd0e206c71ba485e1be6a21627e0a488
Jolene Kaminski <jolene.april14@gmail.com>
2023-10-05 19:03:57 -0500

    Re-wrote seed file to use csv data to populate categories and products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,28 @@
 require 'csv'
 
-676.times do
-  Product.create(
-    title: Faker::Coffee.unique.blend_name,
-    price: Faker::Commerce.price,
-    stock_quantity: Faker::Number.within(range: 1..100)
+Product.destroy_all
+Category.destroy_all
+
+# 676.times do
+#   Product.create(
+#     title: Faker::Coffee.unique.blend_name,
+#     price: Faker::Commerce.price,
+#     stock_quantity: Faker::Number.within(range: 1..100)
+#   )
+# end
+
+filename = Rails.root.join('db/products.csv')
+
+csv_data = File.read(filename)
+products = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+products.each do |p|
+  category = Category.find_or_create_by(name: p['category'])
+
+  product = category.products.create(
+    title: p['name'],
+    description: p['description'],
+    price: p['price'],
+    stock_quantity: p['stock quantity']
   )
 end
